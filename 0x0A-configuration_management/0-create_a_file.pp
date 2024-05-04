83a923a876d8941700b54f114207a83ce866060d
AtlasLioness <narjisaliane@gmail.com>
2024-05-04 20:19:02 +0000

    0. Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet script that creates file in /tmp
+
+file { 'ALX':
+	path => ' /tmp/school',
+	mode => '0744',
+	owner => 'www-data',
+	group => 'www-data',
+	content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,3 @@
+0. Create a file
+1. Install a package
+2. Execute a command
