525bccba7f0894157ac486153fb03e7ffa108ead
AtlasLioness <narjisaliane@gmail.com>
2024-08-13 20:55:20 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """returns nb of subs"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-            "User-Agent": "linux:alx.api:v1.0.0 (by u/Elegant_Student_3795)"
-            }
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
