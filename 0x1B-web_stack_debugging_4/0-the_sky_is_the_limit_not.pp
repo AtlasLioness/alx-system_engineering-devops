0dc59be9998b27119cc5dc0eab3df8d39001fe2d
AtlasLioness <narjisaliane@gmail.com>
2024-08-22 19:39:58 +0000

    0. Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,8 @@
 # Puppet script to increase limit of nginx traffic
 exec { 'fix-for-nginx':
-  command => 'sed -i "s/15/1024/" /etc/default/nginx',
-  path    => '/etc/default/nginx'
+  command => "sed -i 's/-n 15/-n 4096/g' /etc/default/nginx && service nginx restart",
+  onlyif    => 'test -e /etc/default/nginx',
+  provider  => 'shell',
 }
 
 # Nginx restart
